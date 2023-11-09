9c02b16a60f53d34cfd9287e09e2708712cf669b
samogoljat <samo.goljat1@gmail.com>
2023-11-09 06:38:49 +0100

    add

---
+++ backend/routes/places-routes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const placesControllers = require('../controllers/places-controllers')
+
+const HttpError = require("../models/http-error");
+
+const router = express.Router();
+
+
+
+router.get("/:pid", placesControllers.getPlaceById);
+
+router.get("/user/:uid", placesControllers.getPlaceByUserId);
+
+router.post('/', placesControllers.createPlace)
+
+module.exports = router;
